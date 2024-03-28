48d4a3c84818dd60292882dd2341bb2380372cf6
george-prado <gmeloprado@gmail.com>
2024-03-28 11:46:21 -0300

    feat: content component refactored

--- src/app/components/content/content.component.ts
+++ src/app/components/content/content.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrl: './content.component.css'
 })
 export class ContentComponent {
-
+  post = ['flex', 'post-margins']
+  date = ['flex-col', 'date-margins']
+  article = ['flex-col', 'content-width', 'white']
+  title = ['green', 'title']
+  author = ['mb', 'title-font', 'sm-text', 'indent']
+  tagSection = ['btn', 'mb', 'end-margin']
+  tag = ['green-secundary', 'bg']
 }
