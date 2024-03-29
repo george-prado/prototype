49d3c19867201aa363583fca66a6e7dd9e07dd5c
george-prado <gmeloprado@gmail.com>
2024-03-28 21:32:35 -0300

    feat: angular router first implementation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContentComponent } from './components/home/content/content.component';
+import { ContentHeaderComponent } from './components/home/content-header/content-header.component';
+import { FooterComponent } from './components/home/footer/footer.component';
+import { PagenavComponent } from './components/home/pagenav/pagenav.component';
+import { SidenavComponent } from './components/home/sidenav/sidenav.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'home',
+    children: [
+      { path: '', component: ContentComponent },
+      { path: 'content-header', component: ContentHeaderComponent },
+      { path: 'footer', component: FooterComponent },
+      { path: 'pagenav', component: PagenavComponent },
+      { path: 'sidenav', component: SidenavComponent },
+    ]
+  },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: '**', redirectTo: '/home' } 
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
