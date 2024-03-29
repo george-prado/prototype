1641112d707792d9cdbce4441b5cdf0b3b386f47
george-prado <gmeloprado@gmail.com>
2024-03-28 21:31:56 -0300

    feat: minor imports for module animations

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
+import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { MatSidenavContainer, MatSidenavModule } from '@angular/material/sidenav';
-import { SidenavComponent } from './components/sidenav/sidenav.component';
-import { ContentHeaderComponent } from './components/content-header/content-header.component';
-import { ContentComponent } from './components/content/content.component';
-import { FooterComponent } from './components/footer/footer.component';
-import { PagenavComponent } from './components/pagenav/pagenav.component'
+import { SidenavComponent } from './components/home/sidenav/sidenav.component';
+import { ContentHeaderComponent } from './components/home/content-header/content-header.component';
+import { ContentComponent } from './components/home/content/content.component';
+import { FooterComponent } from './components/home/footer/footer.component';
+import { PagenavComponent } from './components/home/pagenav/pagenav.component';
 
 @NgModule({
   declarations: [
@@ -22,14 +23,15 @@ import { PagenavComponent } from './components/pagenav/pagenav.component'
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule,
+    RouterModule,
     MatSidenavModule,
     MatSidenavContainer,
     FormsModule
   ],
   providers: [
-    provideClientHydration(),
-    provideAnimationsAsync()
+    provideClientHydration()
   ],
   bootstrap: [AppComponent]
 })
